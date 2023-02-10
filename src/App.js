d1ce5204b15e10e41aa95a3bd1311b29448754b2
brendonvan <brendon@van.digital>
2023-02-10 14:19:57 -0700

    Add Palette for background color

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-// import * as Vibrant from 'node-vibrant';
-
-// function getPalette() {
-//   Vibrant.from('./images/test.jpg').getPalette((err, palette) => console.log(palette))
-// }
+import * as Vibrant from 'node-vibrant';
+import { useState, useEffect } from 'react';
 
 function App() {
+  let [backgroundColor, setBackgroundColor] = useState('html { background-color: black; }');
+
+  async function getPalette() {
+    //TODO:
+    //TODO grab images uploaded from database
+    setBackgroundColor(Vibrant.from('./images/test.jpg').getPalette((err, palette) => console.log(`html { background-color: rgb(${palette.Vibrant.rgb.toString()}); }`)))
+    await console.log(getPalette())
+  }
+
+  useEffect(() => {
+    getPalette()
+  }, []);
+
   return (
     <HelmetProvider>
       <div className="App" >
         <Helmet>
           <style>{'body { background-color: black; }'}</style>
         </Helmet>
-        {/* <button onClick={ () => getPalette() }></button> */}
       </div>
     </HelmetProvider>
   );
