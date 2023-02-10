0c4b511b91629ac67abb2b1a820c1b5f9f1b4fe2
brendonvan <brendon@van.digital>
2023-02-10 13:42:43 -0700

    Updated Favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+// import * as Vibrant from 'node-vibrant';
+
+// function getPalette() {
+//   Vibrant.from('./images/test.jpg').getPalette((err, palette) => console.log(palette))
+// }
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <HelmetProvider>
+      <div className="App" >
+        <Helmet>
+          <style>{'body { background-color: black; }'}</style>
+        </Helmet>
+        {/* <button onClick={ () => getPalette() }></button> */}
+      </div>
+    </HelmetProvider>
   );
 }
 
